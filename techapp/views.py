189fdfd1420384ee3f3627445613fbb48ef63e0b
Jenis <jeniswinsta@gmail.com>
2023-11-16 09:03:30 +0530

    Post method in forms

--- techapp/views.py
+++ techapp/views.py
@@ -6,8 +6,10 @@ def login(request):
     return render(request,'login.html')
 
 def dashboard(request):
-    username=request.GET['username']
+    username=request.POST['username']
     return render(request, 'dashboard.html',{'name':username})
 
 def register(request):
+    # username=request.POST['username']
+    # password=request.POST['password']
     return render(request, 'register.html')
